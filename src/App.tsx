59c485fdbe044b546f57cd3c8140796325e5f720
brito101 <rodrigobrito101@hotmail.com>
2024-03-07 16:09:35 -0300

    Init final project course

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,17 @@
-import React from "react";
-import useFetch from "./useFetch";
-
-type Product = {
-  id: string;
-  nome: string;
-  descricao: string;
-  quantidade: number;
-  preco: number;
-  internacional: boolean;
-};
-
-const PRODUCTS_URL = "https://data.origamid.dev/produtos";
+import Header from "./Components/Header";
+import SideNav from "./Components/SideNav";
+import Resume from "./Pages/Resume";
+import "./Style.css";
 
 function App() {
-  const [id, setId] = React.useState("p001");
-  const product = useFetch<Product>(`${PRODUCTS_URL}/${id}/`);
-  const products = useFetch<Product[]>(PRODUCTS_URL);
-
   return (
-    <section className="flex">
-      <div>
-        {products.data &&
-          products.data.map((product) => (
-            <button
-              style={{ fontSize: "1rem" }}
-              key={product.id}
-              onClick={() => setId(product.id)}
-            >
-              {product.id}
-            </button>
-          ))}
-      </div>
-      <div>
-        {product.loading && <div>Loading...</div>}
-        {product.data && (
-          <ul>
-            <li>ID: {product.data.id}</li>
-            <li>Name: {product.data.nome}</li>
-            <li>Quantity: {product.data.quantidade}</li>
-            <li>Description: {product.data.descricao}</li>
-            {product.data.internacional ? <li>Origin: International</li> : ""}
-          </ul>
-        )}
-      </div>
-    </section>
+    <div>
+      <SideNav />
+      <main>
+        <Header />
+        <Resume />
+      </main>
+    </div>
   );
 }
 
